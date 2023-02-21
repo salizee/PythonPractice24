41c1255d2c529e0864e58be3d3179f38c0304e10
Lukman Aliyu <aliyulukman94@gmail.com>
2023-02-21 06:32:43 +0100

    update

--- day_017/day_17_exercises.py
+++ day_017/day_17_exercises.py
@@ -8,10 +8,12 @@ Zip
 '''
 
 
-#names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
-# Unpack the first five countries and store them in a variable nordic_countries, 
-# store Estonia and Russia in es, and ru respectively.
+'''
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+Unpack the first five countries and store them in a variable nordic_countries, 
+store Estonia and Russia in es, and ru respectively.'''
 
 names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
 *nordic_countries,es,ru = names
 print(f'The Nordic countries include: {nordic_countries} ')
+print(f'es: {es}, ru: {ru}')
