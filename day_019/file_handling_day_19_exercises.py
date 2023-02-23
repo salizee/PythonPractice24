225cbb189e4836beda08511ca1a6b0ad2e2656f4
Lukman Aliyu <aliyulukman94@gmail.com>
2023-02-23 16:13:59 +0100

    update

--- day_019/file_handling_day_19_exercises.py
+++ day_019/file_handling_day_19_exercises.py
@@ -213,7 +213,7 @@ clean_text('day_019/michelle_obama_speech.txt') # worked
 def remove_stop_words(list):
     '''Removes stop words, i.e. commonly used words that search engines are programmed to ignore, from a list of words'''
     stop_words = ['i','me','my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up','down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
-    return [word for word in list if word not in stop_words]
+    return [word for word in list if word.lower() not in stop_words]
 
 remove_stop_words(clean_text('day_019/michelle_obama_speech.txt'))  # handled
 
@@ -233,7 +233,7 @@ def comparing_text_in_file_similarity(file_one,file_two):
     file_two_words = remove_stop_words(clean_text(file_two))
     return check_text_similarity(file_one_words,file_two_words)
 
-comparing_text_in_file_similarity('day_019/michelle_obama_speech.txt','day_019/melania_trump_speech.txt') # Got 45.57% similarity. 
+comparing_text_in_file_similarity('day_019/michelle_obama_speech.txt','day_019/melania_trump_speech.txt') # Got 41.73% similarity. 
 # Find the 10 most repeated words in the romeo_and_juliet.txt
 # most common and most repeated are the same
 
