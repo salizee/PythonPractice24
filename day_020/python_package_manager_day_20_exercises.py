c6fb2a4461f96ace5ef6129173b19ffa8820ee9a
Lukman Aliyu <aliyulukman94@gmail.com>
2023-02-24 07:40:40 +0100

    typos

--- day_020/python_package_manager_day_20_exercises.py
+++ day_020/python_package_manager_day_20_exercises.py
@@ -117,7 +117,7 @@ the total number of languages in the countries API
 # Webpage is not working!
 
 UCI is one of the most common places to get data sets for data science and machine learning.
-Read the content of UCL (https://archive.ics.uci.edu/ml/datasets.php). 
+Read the content of UCI (https://archive.ics.uci.edu/ml/datasets.php). 
 Without additional libraries it will be difficult, so you may try it with BeautifulSoup4
  '''
 
